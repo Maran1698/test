c28c33512ac7cd5d49538b5850386b22bdc6b3fb
Maran1698 <maran1698@gmail.com>
2023-11-20 22:41:08 +0530

    Update automatic random password generator.py

--- automatic random password generator.py
+++ automatic random password generator.py
@@ -4,10 +4,12 @@ import random
 print("welcome to automatic  random password genateror")
 #password characters
 auto_char=("zxcvbnmlkjhgfdsaqwertyuiop1234567890QWERTYUIOPLKJHGFDSAZXCVBNM!@#$%&*")
-no_of_passwords=int(input('number of automatic random passwords: '))
-len_of_passwords=int(input('lenth of automatic random passwords: '))
-print("automatic random passwords:")
+#input values
+no_of_passwords=int(input('number of automatic random passwords: '))# no of passwords wanted
+len_of_passwords=int(input('lenth of automatic random passwords: '))#lenth of password
 
+print("automatic random passwords:")
+#loop for the genarate password
 for x in range(no_of_passwords):
     password=' '
     for y in range(len_of_passwords):
