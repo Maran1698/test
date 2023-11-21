eb0ebc241ec420efd40c96b2bb2bc9acd9faba36
Maran1698 <maran1698@gmail.com>
2023-11-21 22:06:15 +0530

    second commit

---
+++ quiz game.py
@@ -0,0 +1,32 @@
+print("welcome to my quiz game!")
+
+player = input("Do you want to play? ")
+
+if player.lower() != "yes":
+    quit()
+
+print("Let's start the game :)")
+score = 0
+
+answer = input("What does IAS stand for? ")
+if answer.lower() == "indian administrative service":
+    print("Correct!")
+    score += 1
+else:
+    print("Better luck next time.")
+
+answer = input("What does IPS stand for? ")
+if answer.lower() == "indian police service":
+    print("Correct!")
+    score += 1
+else:
+    print("Better luck next time.")
+
+answer = input("What does 6 + 9 equal? ")
+if int(answer) == 15:
+    print("Correct!")
+    score += 1
+else:
+    print("Better luck next time.")
+
+print("Thank you! You got " + str(score) + " questions correct :)")
